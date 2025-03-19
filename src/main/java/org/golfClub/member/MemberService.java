f1dc6cbd8fe650392d2ea2bf1cedf20a2809e6e1
Victoria Breen <victoriabreen8@hotmail.com>
2025-03-19 16:49:05 -0230

    Fixed a spelling error for a table name

--- src/main/java/org/golfClub/member/MemberService.java
+++ src/main/java/org/golfClub/member/MemberService.java
@@ -1,4 +1,46 @@
 package org.golfClub.member;
 
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+// save - delete - add - all
+//
+@Service
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
+    // Id - might be more
+    public Optional<Member> getMemberByMemberId(Long id) {
+        return memberRepository.findById(id);
+    }
+
+    public void deleteMemberById(Long id) {
+        memberRepository.deleteById(id);
+    }
+
+    public boolean checkIfMemberExist(Long id) {
+        return memberRepository.existsById(id);
+    }
+
+    // String
+    public List<Member> getMembersByFirstName(String firstName) {
+        return memberRepository.findByFirstName(firstName);
+    }
+
+    public List<Member> getMembersBylasttName(String lastName) {
+        return memberRepository.findByLastName(lastName);
+    }
+
+    public List<Member> getMembersByFullName(String firstName, String lastName) {
+        return memberRepository.findByFullName(firstName, lastName);
+    }
+
 }
