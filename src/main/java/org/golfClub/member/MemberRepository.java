e39b720fd7fce53fd8a1857053e4790fb1f057e5
Victoria Breen <victoriabreen8@hotmail.com>
2025-03-19 16:05:31 -0230

    Building repositories

--- src/main/java/org/golfClub/member/MemberRepository.java
+++ src/main/java/org/golfClub/member/MemberRepository.java
@@ -1,4 +1,15 @@
 package org.golfClub.member;
 
-public class MemberRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByFirstName(String firstName);
+    List<Member> findByLastName(String lastName);
+    List<Member> findByFullName(String firstName, String lastName);
+    List<Member> findByPhoneNumber(String phoneNum);
+    List<Member> findByMemberStartDate(String memberStartDate);
 }
--- src/main/java/org/golfClub/tournament/TournamentRepository.java
+++ src/main/java/org/golfClub/tournament/TournamentRepository.java
@@ -1,4 +1,15 @@
 package org.golfClub.tournament;
 
-public class TournamentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface TournamentRepository extends JpaRepository<Tournament, Long> {
+    List<Tournament> findByStartDate(LocalDate startDate);
+    List<Tournament> findByLocation(String location);
+    Tournament findByIdAndParticipants(Long tournamentId);
 }
+
